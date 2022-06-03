82cd7bc5bde0929707d1a2764b76990804c7bd0f
lucas-wa <lucaswanderley600@gmail.com>
2022-06-03 16:22:15 -0300

    aula02

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -4,10 +4,18 @@ module.exports = {
     extend: {
       colors : {
         brand: {
+          300: '#996DFF',
           500: '#8257e6',
         }
       },
+
+      borderRadius: {
+        md: `4px` // Sobreescreve uma propriedade do tailwind
+      }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar'),
+  ],
 }
